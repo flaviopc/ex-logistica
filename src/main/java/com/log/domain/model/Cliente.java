45bb81699b69dc814fc4529aa7d2c89f82cf0d2e
Flávio Paixão <flavio_pc@outlook.com>
2022-01-13 20:15:03 -0300

    model cliente

---
+++ src/main/java/com/log/domain/model/Cliente.java
@@ -0,0 +1,27 @@
+package com.log.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+public class Cliente {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+}
